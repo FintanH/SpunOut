6a81f73fc5e5d06ed7d139e717e106ea249a9405
Conor Brady <conorjbrady1@gmail.com>
2014-11-22 14:31:53 +0000

    templates added

---
+++ templates.js
@@ -0,0 +1,24 @@
+<script id="User-Story" type="text/javascript">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">{{Title}}</h3>
+					</div>
+					<div class="panel-body">
+						{{TitleContent}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="well">
+			<p>
+				{{Description}}
+			</p>
+		</div>
+	</div>
+</script>
